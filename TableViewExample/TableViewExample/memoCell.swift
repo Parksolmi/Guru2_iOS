28bd544f778cbb4d11ead5b9826566f7407bf061
Parksolmi <7548ck@naver.com>
2022-07-22 10:29:55 +0900

    테이블 뷰에서 구현 가능한 여러 가지 기능들

---
+++ TableViewExample/TableViewExample/memoCell.swift
@@ -0,0 +1,17 @@
+//
+//  memoCell.swift
+//  TableViewExample
+//
+//  Created by 박솔미 on 2022/07/19.
+//
+
+import UIKit
+
+class memoCell: UITableViewCell {
+    
+    @IBOutlet weak var numLabel: UILabel!
+    @IBOutlet weak var memoLabel: UILabel!
+    
+    
+    
+}
