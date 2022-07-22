0001a01e73f7ba59133cb691ddb22e0bde562984
Parksolmi <7548ck@naver.com>
2022-07-22 16:47:04 +0900

    컬렉션 뷰 구현

---
+++ CollectionViewExample/CollectionViewExample/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  CollectionViewExample
+//
+//  Created by 박솔미 on 2022/07/22.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var btn: UIButton!
+    
+}
---
+++ CollectionViewExample/CollectionViewExample/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  CollectionViewExample
+//
+//  Created by 박솔미 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Cell
+        
+        cell.btn.setTitle("\(indexPath.row)", for: .normal)
+        
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    //cell끼리의 상하 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+    
+    //cell끼리의 좌우 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+    
+    //cell의 너비
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (collectionView.frame.width - 60) / 3
+        let height = width * 1.6
+        let size = CGSize(width: width, height: height)
+        return size
+    }
+}
+
