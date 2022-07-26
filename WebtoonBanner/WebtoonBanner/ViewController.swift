416a13239798569a6ed1bde55b8e94a6ae895f25
Parksolmi <7548ck@naver.com>
2022-07-26 21:23:34 +0900

    가로 ScrollView로 만든 웹툰 배너

---
+++ WebtoonBanner/WebtoonBanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebtoonBanner
+//
+//  Created by 박솔미 on 2022/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
