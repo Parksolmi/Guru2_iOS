2203694abf42454bd569e73cc537313586c65769
Parksolmi <7548ck@naver.com>
2022-07-30 17:07:50 +0900

    네이버 웹툰 클론코딩

--- NaverWebtoonExample/NaverWebtoonExample/ViewControllers/ViewController.swift
+++ NaverWebtoonExample/NaverWebtoonExample/ViewControllers/ViewController.swift
@@ -25,6 +25,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //네비게이션 바
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            self.navigationController?.setNavigationBarHidden(true, animated: false)
+        }
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            self.navigationController?.setNavigationBarHidden(false, animated: false)
+        }
+    
 }
 
 extension ViewController: UICollectionViewDataSource {
